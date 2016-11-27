0fc402a85f1f7bbba3c0ddae553c545b34bce6bf
muqsith <muqsithirfan@gmail.com>
2016-11-27 23:17:49 +0530

    Added ideas of improvement (2)

--- src/game.jsx
+++ src/game.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Square(props){
     return (
-      <button className="square" onClick={ () => props.onClick() }>
+      <button className={props.win
+              ? "square winning-square":"square"}
+              onClick={ () => props.onClick() }>
           {props.value}
       </button>
     );
@@ -11,8 +13,9 @@ function Square(props){
 class Board extends React.Component {
 
     renderSquare(i) {
-        return <Square value={this.props.squares[i]}
+        return <Square value={this.props.squares[i].value}
             onClick={ () => this.props.onClick(i) }
+            win={this.props.squares[i].win}
             key={i}
             />;
     };
@@ -55,16 +58,21 @@ export default class Game extends React.Component {
             selectedLink: null,
             size: 3
         };
+        let _squares = [];
+        for(var i=0; i<(this.state.size*this.state.size); i+=1){
+            _squares.push({value:null, win:false});
+        }
+        this.state.history[0].squares = _squares;
     };
 
     handleClick(i) {
         const history = this.state.history.slice(0,this.state.stepNumber+1);
         const current = history[history.length - 1];
         const squares = JSON.parse(JSON.stringify(current.squares.slice()));
-        if(calculateWinner(squares) || squares[i]){
+        if(calculateWinner(squares) || squares[i].value){
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        squares[i].value = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
                 squares: squares,
@@ -73,6 +81,7 @@ export default class Game extends React.Component {
             xIsNext: !this.state.xIsNext,
             stepNumber: history.length,
             selectedLink: null,
+            movesSort : 1,
             size: 3
         });
     };
@@ -92,7 +101,26 @@ export default class Game extends React.Component {
             boldClass = 'text-bold';
         }
         return boldClass;
-    }
+    };
+
+    sortMoves() {
+        var s = this.state.history.slice(0);
+        s.sort((a,b) => {
+            if(this.state.movesSort === 1){
+                return b.squareIndex - a.squareIndex;
+            }else{
+                return a.squareIndex - b.squareIndex;
+            }
+        });
+        if(this.state.movesSort === 1){
+            this.state.movesSort = -1;
+        }else{
+            this.state.movesSort = 1;
+        }
+        this.setState({
+            history : s
+        });
+    };
 
   render() {
       const history = this.state.history;
@@ -105,17 +133,7 @@ export default class Game extends React.Component {
       } else {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
-      const moves = history.map((step, move) => {
-          var [row,col] = getPosition(this.state.size, step.squareIndex);
-          const desc = move ?
-            'Move #(' +row+','+col+')' :
-            'Game start';
-          return (
-            <li className={ this.boldClass(move) } key={move}>
-              <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
-            </li>
-          );
-      });
+
     return (
       <div className="game">
         <div className="game-board">
@@ -127,7 +145,25 @@ export default class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{moves}</ol>
+          <div>
+            <button className="sort-button"
+                  onClick={ () => this.sortMoves() }>Sort moves - Toggle</button>
+            <ol>
+                {
+                    this.state.history.map((step, move) => {
+                        var [row,col] = getPosition(this.state.size, step.squareIndex);
+                        const desc = move ?
+                          'Move #(' +row+','+col+')' :
+                          'Game start';
+                          return (
+                          <li className={ this.boldClass(move) } key={move}>
+                              <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+                          </li>
+                      );
+                    })
+                }
+            </ol>
+          </div>
         </div>
       </div>
     );
@@ -154,10 +190,13 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a]
-        && squares[a] === squares[b]
-        && squares[a] === squares[c]) {
-      return squares[a];
+    if (squares[a] && squares[a].value
+        && squares[a].value === squares[b].value
+        && squares[a].value === squares[c].value) {
+        squares[a].win = true;
+        squares[b].win = true;
+        squares[c].win = true;
+      return squares[a].value;
     }
   }
   return null;
